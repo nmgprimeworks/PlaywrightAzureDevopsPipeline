23385e1756d8994e44d1d56b20c03ea2f2e371ef
nmgprimeworks <156620497+nmgprimeworks@users.noreply.github.com>
2024-03-05 10:38:53 -0500

    Update demo.spec.js

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -16,7 +16,7 @@ test('Google test1', async({page}) =>{
  await page.locator('#container > #thumbnail').first().click();
  await page.waitForTimeout(5000);
 })
-
+/*
 // Write a test
 test('Google test2', async({page}) =>{
   // Go to URL
@@ -30,7 +30,7 @@ test('Google test2', async({page}) =>{
  // Validate title
  await expect(page).toHaveTitle(' Playwright Tutorial Full Course 2024 | Playwright Testing Tutorial')
  
-})
+})*/
 
 test("API Test1", async ({ request}) => {
   // create post api request using playwright
@@ -97,7 +97,7 @@ test("API Test3", async ({ request}) => {
   expect(postAPIResponse.ok()).toBeTruthy();
   expect(postAPIResponse.status()).toBe(200);
 });
-
+/*
 test("API Test4", async ({ request}) => {
   // create post api request using playwright
   const postAPIResponse = await request.post("https://restful-booker.herokuapp.com/booking", {
@@ -113,7 +113,7 @@ test("API Test4", async ({ request}) => {
       additionalneeds: "super bowls",
     },
   });
-
+*/
   // validate status code
   console.log(await postAPIResponse.json());
   expect(postAPIResponse.ok()).toBeTruthy();
